83b3ec6abf729fadfd86bc3a0a4a77283cb4e841
Terry Lorber <tgl@rideside.net>
2009-03-19 04:02:53 +0000

    adding paperclipped

---
+++ vendor/extensions/paperclipped/app/models/asset.rb
@@ -0,0 +1,144 @@
+class Asset < ActiveRecord::Base
+  # used for extra mime types that dont follow the convention
+  @@image_content_types = ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png', 'image/jpg']
+  @@extra_content_types = { :audio => ['application/ogg'], :movie => ['application/x-shockwave-flash'], :pdf => ['application/pdf'] }.freeze
+  cattr_reader :extra_content_types, :image_content_types
+
+  # use #send due to a ruby 1.8.2 issue
+  @@image_condition = send(:sanitize_sql, ['asset_content_type IN (?)', image_content_types]).freeze
+  @@movie_condition = send(:sanitize_sql, ['asset_content_type LIKE ? OR asset_content_type IN (?)', 'video%', extra_content_types[:movie]]).freeze
+  @@audio_condition = send(:sanitize_sql, ['asset_content_type LIKE ? OR asset_content_type IN (?)', 'audio%', extra_content_types[:audio]]).freeze
+  
+  @@other_condition = send(:sanitize_sql, [
+    'asset_content_type NOT LIKE ? AND asset_content_type NOT LIKE ? AND asset_content_type NOT IN (?)',
+    'audio%', 'video%', (extra_content_types[:movie] + extra_content_types[:audio] + image_content_types)]).freeze
+  cattr_reader *%w(movie audio image other).collect! { |t| "#{t}_condition".to_sym }
+  
+  
+  class << self
+    def image?(asset_content_type)
+      image_content_types.include?(asset_content_type)
+    end
+    
+    def movie?(asset_content_type)
+      asset_content_type.to_s =~ /^video/ || extra_content_types[:movie].include?(asset_content_type)
+    end
+        
+    def audio?(asset_content_type)
+      asset_content_type.to_s =~ /^audio/ || extra_content_types[:audio].include?(asset_content_type)
+    end
+    
+    def other?(asset_content_type)
+      ![:image, :movie, :audio].any? { |a| send("#{a}?", asset_content_type) }
+    end
+
+    def pdf?(asset_content_type)
+      extra_content_types[:pdf].include? asset_content_type
+    end
+
+    def find_all_by_content_types(types, *args)
+      with_content_types(types) { find *args }
+    end
+
+    def with_content_types(types, &block)
+      with_scope(:find => { :conditions => types_to_conditions(types).join(' OR ') }, &block)
+    end
+
+    def types_to_conditions(types)
+      types.collect! { |t| '(' + send("#{t}_condition") + ')' }
+    end
+  end
+  
+  
+  order_by 'title'
+  
+  if Radiant::Config.table_exists? && Radiant::Config["assets.additional_thumbnails"]
+    thumbnails = Radiant::Config["assets.additional_thumbnails"].split(', ').collect{|s| s.split('=')}.inject({}) {|ha, (k, v)| ha[k.to_sym] = v; ha}
+  else
+    thumbnails = {}
+  end
+  thumbnails[:icon] = ['42x42#', :png]
+  thumbnails[:thumbnail] = ['100x100>', :png]
+  
+  has_attached_file :asset,
+                    :styles => thumbnails,
+                    :whiny_thumbnails => false,
+                    :url => "/:class/:id/:basename:no_original_style.:extension",
+                    :path => ":rails_root/public/:class/:id/:basename:no_original_style.:extension"
+                                 
+  has_many :page_attachments, :dependent => :destroy
+  has_many :pages, :through => :page_attachments
+                                 
+  belongs_to :created_by, :class_name => 'User'
+  belongs_to :updated_by, :class_name => 'User'
+  
+  validates_attachment_presence :asset, :message => "You must choose a file to upload!"
+  validates_attachment_content_type :asset, 
+    :content_type => Radiant::Config["assets.content_types"].split(', ') if Radiant::Config.table_exists? && Radiant::Config["assets.content_types"] && Radiant::Config["assets.skip_filetype_validation"] == nil
+  validates_attachment_size :asset, 
+    :less_than => Radiant::Config["assets.max_asset_size"].to_i.megabytes if Radiant::Config.table_exists? && Radiant::Config["assets.max_asset_size"]
+    
+    
+  before_save :assign_title
+  
+  
+  
+  def thumbnail(size = nil)
+    if size == 'original' or size.nil?
+      self.asset.url
+    else
+      if self.pdf?
+        "/images/assets/pdf_#{size.to_s}.png"
+      elsif self.movie?
+        "/images/assets/movie_#{size.to_s}.png"
+      elsif self.audio?
+        "/images/assets/audio_#{size.to_s}.png"
+      elsif self.other?
+        "/images/assets/doc_#{size.to_s}.png"
+      else
+        self.asset.url(size.to_sym)
+      end
+    end
+  end
+  
+  def generate_thumbnail(name, args)
+    path = File.join(RAILS_ROOT, 'public', self.asset(:original))
+    self.asset do 
+      path = File.join(RAILS_ROOT, 'public', self.asset(:original))
+      begin
+        dimensions, format = args
+        dimensions = dimensions.call(instance) if dimensions.respond_to? :call
+        @queued_for_write[name] = Paperclip::Thumbnail.make(File.new(path), 
+                                                 dimensions,
+                                                 format, 
+                                                 @whiny_thumnails)
+      rescue PaperclipError => e
+        @errors << e.message if @whiny_thumbnails
+      end
+      attachment.save
+    end
+  end
+  
+  def basename
+    File.basename(asset_file_name, ".*") if asset_file_name
+  end
+  
+  def extension
+    asset_file_name.split('.').last.downcase if asset_file_name
+  end
+  
+  [:movie, :audio, :image, :other, :pdf].each do |content|
+    define_method("#{content}?") { self.class.send("#{content}?", asset_content_type) }
+  end
+  
+  
+  private
+  
+    def assign_title
+      self.title = basename if title.blank?
+    end
+    
+    def additional_thumbnails
+      Radiant::Config["assets.additional_thumbnails"].split(',').collect{|s| s.split('=')}.inject({}) {|ha, (k.to_sym, v)| ha[k] = v; ha}
+    end
+end
---
+++ vendor/extensions/paperclipped/app/models/asset_tags.rb
@@ -0,0 +1,173 @@
+module AssetTags
+  include Radiant::Taggable
+  
+  class TagError < StandardError; end
+  
+  desc %{
+    The namespace for referencing images and assets.  You may specify the 'title'
+    attribute on this tag for all contained tags to refer to that asset.  
+    
+    *Usage:* 
+    <pre><code><r:assets [title="asset_title"]>...</r:assets></code></pre>
+  }    
+  tag 'assets' do |tag|
+    tag.locals.asset = Asset.find_by_title(tag.attr['title']) || Asset.find(tag.attr['id']) unless tag.attr.empty?
+    tag.expand
+  end
+  
+  desc %{
+    Cycles through all assets attached to the current page.  
+    This tag does not require the title atttribute, nor do any of its children.
+    Use the `limit' attribute to render a specific number of assets.
+    
+    *Usage:* 
+    <pre><code><r:assets:each [limit="5"]>...</r:assets:each></code></pre>
+  }    
+  tag 'assets:each' do |tag|
+    options = tag.attr.dup
+    result = []
+    limit = options['limit'] ? options.delete('limit') : nil
+    attachments = tag.locals.page.page_attachments
+    attachments = attachments.find(:all, :limit => limit) if limit
+    attachments.each do |attachment|
+      tag.locals.asset = attachment.asset
+      result << tag.expand
+    end
+    result
+  end
+  
+  desc %{
+    References the first asset attached to the current page.  
+    
+    *Usage:* 
+    <pre><code><r:assets:first>...</r:assets:first></code></pre>
+  }
+  tag 'assets:first' do |tag|
+     attachments = tag.locals.page.page_attachments
+     if first = attachments.first
+       tag.locals.asset = first.asset
+       tag.expand
+     end
+   end
+   
+   tag 'assets:if_first' do |tag|
+     attachments = tag.locals.assets
+     asset = tag.locals.asset
+     if asset == attachments.first.asset
+       tag.expand
+     end
+   end
+
+  desc %{
+    Renders the containing elements only if the asset's content type matches the regular expression given in the matches attribute.
+    The 'title' attribute is required on the parent tag unless this tag is used in assets:each.
+    If the 'ignore_case' attribute is set to false, the match is case sensitive. By default, 'ignore_case' is set to true.
+
+    *Usage:* 
+    <pre><code><r:assets:each:if_content_type matches="regexp" [ignore_case=true|false"]>...</r:assets:each:if_content_type></code></pre>
+  }
+  tag 'assets:if_content_type' do |tag|
+    options = tag.attr.dup
+    # XXX build_regexp_for comes from StandardTags
+    # XXX its cool if I use it, right?
+    regexp = build_regexp_for(tag,options)
+    asset_content_type = tag.locals.asset.asset_content_type
+    tag.expand unless asset_content_type.match(regexp).nil?
+  end
+  
+  [:title, :caption, :asset_file_name, :asset_content_type, :asset_file_size, :id].each do |method|
+    desc %{
+      Renders the `#{method.to_s}' attribute of the asset.     
+      The 'title' attribute is required on this tag or the parent tag.
+    }
+    tag "assets:#{method.to_s}" do |tag|
+      options = tag.attr.dup
+      asset = find_asset(tag, options)
+      asset.send(method) rescue nil
+    end
+  end
+  
+  tag "assets:filename" do |tag|
+    options = tag.attr.dup
+    asset = find_asset(tag, options)
+    asset.asset_file_name rescue nil
+  end
+  
+  desc %{
+    Renders an image tag for the asset. Using the option size attribute, different sizes can be display. Thumbnail and icon are built 
+    in, but custom sizes can be set using assets.addition_thumbnails in the Radiant::Config settings.
+    
+    *Usage:* 
+    <pre><code><r:assets:image [title="asset_title"] [size="icon|thumbnail"]></code></pre>
+  }    
+  tag 'assets:image' do |tag|
+    options = tag.attr.dup
+    asset = find_asset(tag, options)
+    if asset.image?
+      size = options['size'] ? options.delete('size') : 'original'
+      alt = " alt='#{asset.title}'" unless tag.attr['alt'] rescue nil
+      attributes = options.inject('') { |s, (k, v)| s << %{#{k.downcase}="#{v}" } }.strip
+      attributes << alt unless alt.nil?
+      url = asset.thumbnail(size)
+      %{<img src="#{url}" #{attributes unless attributes.empty?} />} rescue nil
+    else
+      raise TagError, "Asset is not an image"
+    end
+  end
+  
+  tag 'assets:thumbnail' do |tag|
+    options = tag.attr.dup
+    asset = find_asset(tag, options)
+    asset.generate_thumbnail('test', ['24x24#',nil])
+    asset.save    
+  end
+  
+  desc %{
+    Renders the url for the asset. If the asset is an image, the <code>size</code> attribute can be used to 
+    generate the url for that size. 
+    
+    *Usage:* 
+    <pre><code><r:image [title="asset_title"] [size="icon|thumbnail"]></code></pre>
+  }    
+  tag 'assets:url' do |tag|
+    options = tag.attr.dup
+    asset = find_asset(tag, options)
+    size = options['size'] ? options.delete('size') : 'original'
+    asset.thumbnail(size) rescue nil
+  end
+  
+  desc %{
+    Renders a link to the asset. If the asset is an image, the <code>size</code> attribute can be used to 
+    generate a link to that size. 
+    
+    *Usage:* 
+    <pre><code><r:image [title="asset_title"] [size="icon|thumbnail"]></code></pre>
+  }
+  tag 'assets:link' do |tag|
+    options = tag.attr.dup
+    asset = find_asset(tag, options)
+    size = options['size'] ? options.delete('size') : 'original'
+    text = options['text'] || asset.title
+    anchor = options['anchor'] ? "##{options.delete('anchor')}" : ''
+    attributes = options.inject('') { |s, (k, v)| s << %{#{k.downcase}="#{v}" } }.strip
+    attributes = " #{attributes}" unless attributes.empty?
+    text = tag.double? ? tag.expand : text
+    url = asset.thumbnail(size)
+    %{<a href="#{url  }#{anchor}"#{attributes}>#{text}</a>} rescue nil
+  end
+  
+  # Resets the page Url and title within the asset tag
+  [:title, :url].each do |method|
+    tag "assets:page:#{method.to_s}" do |tag|
+      tag.locals.page.send(method)
+    end
+  end
+  
+  private
+    
+    def find_asset(tag, options)
+      raise TagError, "'title' attribute required" unless title = options.delete('title') or id = options.delete('id') or tag.locals.asset
+      tag.locals.asset || Asset.find_by_title(title) || Asset.find(id)
+    end
+    
+end
