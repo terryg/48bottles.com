e6c8ddaed18e9536f2c936326959527d3711d162
Terry Lorber <tgl@rideside.net>
2010-01-04 00:00:16 +0000

    ENH removing radiant

--- vendor/extensions/paperclipped/lib/url_additions.rb
+++
@@ -1,9 +0,0 @@
-module UrlAdditions
-  
-  Paperclip::Attachment.interpolations[:no_original_style] = lambda do |attachment, style|
-    style == attachment.default_style ? nil : "_#{style}"
-  end
-  
-end
-  
-
