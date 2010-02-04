2164d20254799126a4a9dcb52faa47bbf54205f4
Terry Lorber <tgl@rideside.net>
2010-02-04 03:27:24 +0000

    ENH add google adsense

--- wp-content/themes/simplish/sidebar.php
+++ wp-content/themes/simplish/sidebar.php
@@ -1,4 +1,15 @@
 		<div id="sidebar">
+<script type="text/javascript"><!--
+google_ad_client = "pub-7789131984925279";
+/* 728x15, created 2/3/10 */
+google_ad_slot = "8746594785";
+google_ad_width = 728;
+google_ad_height = 15;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
 			<ul>
 			<?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar(1) ) : else : ?>
 				<li id="search" class="search">
